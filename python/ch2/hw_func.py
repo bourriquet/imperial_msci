d040022fe142c28c2b399f77078c7604bf47c4c7
Lauren Anders <laurenanders92@gmail.com>
2016-02-14 20:30:30 +0000

    Create hw_func.py

---
+++ python/ch2/hw_func.py
@@ -0,0 +1,12 @@
+def hw1():
+    return "Hello, World!"
+
+def hw2():
+    print "Hello, World!"
+    
+def hw3(str1, str2):
+    print str1,",", str2
+    
+print hw1()
+hw2()
+hw3("Hello", "World!")
