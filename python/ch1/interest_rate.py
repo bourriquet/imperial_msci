8aa46fcb72d9af30b10d03205402516db9919104
Lauren Anders <laurenanders92@gmail.com>
2016-02-14 20:05:49 +0000

    Update interest_rate.py

--- python/ch1/interest_rate.py
+++ python/ch1/interest_rate.py
@@ -1,6 +1,7 @@
 A = 1000
 p = 0.05
+n = 3
 
-money = A * (1 + (p/100))**3
+money = A * (1 + (p/100))**n
 
-print "1000 euros has grown to %.2f euros after 3 years with a 0.05 interest rate" %(money)
+print "%g euros has grown to %.2f euros after %g years with a %g interest rate" %(A, money, n, p)
