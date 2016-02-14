9fb99c1a1baac04424453cae3c4b7c64586d9497
Lauren Anders <laurenanders92@gmail.com>
2016-02-14 20:00:39 +0000

    Update formulas_shapes.py

--- python/ch1/formulas_shapes.py
+++ python/ch1/formulas_shapes.py
@@ -11,7 +11,7 @@ area_square = b**2
 print "The area of the square is %g" %(area_square)
 
 area_circle = pi*(r**2)
-print "The area of the circle is %8.3f" %(area_circle)
+print "The area of the circle is %.3f" %(area_circle)
 
 volume_cone = (1.0/3.0)*pi*(r**2)*h
 print "The volume of the cone is %.3f" %(volume_cone)
